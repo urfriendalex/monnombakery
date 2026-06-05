d0b3279f0dcec485867556c02c688a5758eed6ac
urfriendalex <45111112+urfriendalex@users.noreply.github.com>
2026-06-05 17:40:10 +0200

    Update Sanity project ID in configuration file to reflect new environment variable.

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { schemaTypes } from "./sanity/schemas";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "replace-me";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "af0oyl40";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production";
 
 export default defineConfig({
